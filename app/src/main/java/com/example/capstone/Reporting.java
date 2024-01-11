4c060f345c39e7dc86308d9e7aa039d4d30dedbb
dragosh0705 <pingu0705@hanyang.ac.kr>
2024-01-11 19:21:20 +0900

    2020_01_11

---
+++ app/src/main/java/com/example/capstone/Reporting.java
@@ -0,0 +1,33 @@
+package com.example.capstone;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Reporting extends AppCompatActivity {
+    EditText reportphonenum = findViewById(R.id.OptionalPhoneNumber);
+    EditText reportreason = findViewById(R.id.StoryBox);
+    Button reportButton = findViewById(R.id.EndWrite);
+    String StringReportPhoneNum = reportphonenum.getText().toString();
+    String StringReportReason = reportreason.getText().toString();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.reportpage);
+        if (StringReportPhoneNum != "" && StringReportReason != "") {
+            reportButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent (getApplicationContext(), endreporting.class);
+                    startActivity(intent);
+                }
+            });
+        }
+    }
+
+
+}
---
+++ app/src/main/java/com/example/capstone/endreporting.java
@@ -0,0 +1,12 @@
+package com.example.capstone;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class endreporting extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.endreport);
+    }
+}
