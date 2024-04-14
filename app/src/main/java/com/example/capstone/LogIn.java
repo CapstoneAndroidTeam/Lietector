0ae57f71eb0852580001c847403f3ed55edb4ad1
Yoonkyung Kim <kyk709@hanyang.ac.kr>
2024-04-14 23:38:17 +0900

    kakaologin

--- app/src/main/java/com/example/capstone/LogIn.java
+++ app/src/main/java/com/example/capstone/LogIn.java
@@ -9,8 +9,12 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.capstone.LogInService;
@@ -18,7 +22,12 @@ import com.example.capstone.LogInSrv;
 import com.example.capstone.MainActivity;
 import com.example.capstone.R;
 import com.example.capstone.SignIn;
+import com.kakao.sdk.auth.model.OAuthToken;
+import com.kakao.sdk.user.UserApiClient;
+import com.kakao.sdk.user.model.User;
 
+import kotlin.Unit;
+import kotlin.jvm.functions.Function2;
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Call;
@@ -28,6 +37,11 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class LogIn extends AppCompatActivity {
+
+    private static final String TAG = "kakaoLogin";
+    private View kakaoBtn;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,6 +56,9 @@ public class LogIn extends AppCompatActivity {
         EditText passwordEditText = findViewById(R.id.PassWordEditText);
         Button signInButton = findViewById(R.id.signinButton);
         Button logInButton = findViewById(R.id.LogInButton);
+
+        kakaoBtn = findViewById(R.id.kakaobtn);
+        updateKakaoLoginUi();
 
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("http://54.180.213.170/login/") // Adjust the base URL as per your backend endpoint
@@ -85,11 +102,48 @@ public class LogIn extends AppCompatActivity {
             }
         });
 
+        Function2<OAuthToken, Throwable, Unit> callback = new  Function2<OAuthToken, Throwable, Unit>() {
+            @Override
+            public Unit invoke(OAuthToken oAuthToken, Throwable throwable) {
+                updateKakaoLoginUi();
+                return null;
+            }
+        };
+        kakaoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(UserApiClient.getInstance().isKakaoTalkLoginAvailable(LogIn.this)) { //카카오톡 설치되어있으면
+                    UserApiClient.getInstance().loginWithKakaoTalk(LogIn.this, callback);
+                }else {
+                    UserApiClient.getInstance().loginWithKakaoAccount(LogIn.this, callback);
+                }
+            }
+        });
+
         signInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent goSignInPage = new Intent(getApplicationContext(), SignIn.class);
                 startActivity(goSignInPage);
+            }
+        });
+    }
+
+    private void updateKakaoLoginUi() {
+        UserApiClient.getInstance().me(new Function2<User, Throwable, Unit>() {
+            @Override
+            public Unit invoke(User user, Throwable throwable) {
+                // 로그인이 되어있으면
+                if (user != null) {
+                    // 유저의 아이디
+                    Log.d(TAG, "invoke: id" + user.getId());
+
+                    kakaoBtn.setVisibility(View.GONE);
+                } else {
+                    // 로그인이 되어 있지 않다면 위와 반대로
+                    kakaoBtn.setVisibility(View.VISIBLE);
+                }
+                return null;
             }
         });
     }
